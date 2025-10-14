8f436965f259d9e75aceaa70f96104ee8283727e
multiful <129200984+multiful@users.noreply.github.com>
2025-10-14 10:49:36 +0900

    Update streamlit.py
    data/추가

--- streamlit.py
+++ streamlit.py
@@ -11,9 +11,9 @@ import streamlit as st
 st.set_page_config(page_title="전공별 자격증 대시보드", layout="wide", page_icon="🎓")
 st.title("🎓 전공별 자격증 난이도·합격률 대시보드")
 
-CERT_PATHS  = ["data_cert.xlsx"]
-MAJOR_PATHS = ["data_major.xlsx"]
-JOBS_PATHS  = ["data_jobs.xlsx"]
+CERT_PATHS  = ["data/data_cert.xlsx"]
+MAJOR_PATHS = ["data/data_major.xlsx"]
+JOBS_PATHS  = ["data/data_jobs.xlsx"]
 
 YEARS  = [2022, 2023, 2024]
 PHASES = ["1차", "2차", "3차"]
@@ -350,5 +350,6 @@ with st.expander("다운로드"):
         file_name="license_filtered.csv",
         mime="text/csv",
     )
+
 
 
